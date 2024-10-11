60e9b60eed83c56d7c77668565a6b36257538fde
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-11 08:50:44 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Board:
+    """
+    Main board class. Sets board size, the number of ships,
+    the players's name and the board type (player board or computer)
+    Has methods for add ships and guesses, and printing the board
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [['.' for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
