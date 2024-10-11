ae53ce391265f179dd5961a107f4bd64fb8062e4
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-10-11 09:21:18 +0000

    Copy battleship class from code institute

--- run.py
+++ run.py
@@ -11,3 +11,29 @@ class Board:
         self.board = [['.' for x in range(size)] for y in range(size)]
         self.num_ships = num_ships
         self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+    
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+    
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "X"
+
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+    
+    def add_ship(self, x, y, type="Computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add anymore ships!")
+        else:
+            self.ships.append((x, y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+    
